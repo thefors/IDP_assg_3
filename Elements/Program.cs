eedb963492fad7738f38a515a3bb6b4c1a8fad5b
Anders Lundholm Fors <mr.fors@gmail.com>
2023-02-24 10:00:39 +0100

    added Element Print() method and implemented it using three elements

--- Elements/Program.cs
+++ Elements/Program.cs
@@ -5,12 +5,14 @@
         
         public class Element
         {
+            // Fields
             public string name;
             public int Z;
             public string category;
             public double meltingPoint;
             public double boilingPoint;
 
+            // Constructor
             public Element(string name, int z, string category, double meltingPoint, double boilingPoint)
             {
                 this.name = name;
@@ -18,6 +20,15 @@
                 this.category = category;
                 this.meltingPoint = meltingPoint;
                 this.boilingPoint = boilingPoint;
+            }
+
+            // Print method for presenting element data
+            public void Print()
+            {
+                Console.WriteLine($"Element: {this.name}");
+                Console.WriteLine($"    category: {this.category}");
+                Console.WriteLine($"    melting point: {this.meltingPoint} K");
+                Console.WriteLine($"    boiling point: {this.boilingPoint} K");
             }
         }
         static void Main(string[] args)
@@ -27,9 +38,14 @@
             Console.WriteLine("===================\n");
 
             // three instances: oxygen, iron and gold
-            Element oxygen = new Element("oxygen", 8, "non-metal", 54.36, 90.188);
-            Element iron = new Element("iron", 26, "metal", 1811, 3134);
-            Element gold = new Element("gold", 79, "metal", 1337.33, 3243);
+            Element oxygen = new Element("Oxygen", 8, "non-metal", 54.36, 90.188);
+            Element iron = new Element("Iron", 26, "metal", 1811, 3134);
+            Element gold = new Element("Gold", 79, "metal", 1337.33, 3243);
+
+            // print element data for oxygen, iron and gold
+            oxygen.Print();
+            iron.Print();
+            gold.Print();
             
         }
     }
