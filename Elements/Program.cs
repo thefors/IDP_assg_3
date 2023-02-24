8e0594f01ffc9bd24eff40a3481ba05683a41918
Anders Lundholm Fors <mr.fors@gmail.com>
2023-02-24 09:36:10 +0100

    Add project files.

---
+++ Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Elements
+{
+    internal class Program
+    {
+        
+        public class Element
+        {
+            public string name;
+            public int Z;
+            public string category;
+            public int meltingPoint;
+            public int boilingPoint;
+
+            public Element(string name, int z, string category, int meltingPoint, int boilingPoint)
+            {
+                this.name = name;
+                this.Z = z;
+                this.category = category;
+                this.meltingPoint = meltingPoint;
+                this.boilingPoint = boilingPoint;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Elements");
+            Console.WriteLine("===================\n");
+        }
+    }
+}
